dc906e0875bff1e1e311e635c6ec39dfa54a349f
Tomás Badenes <tomasbadenes@gmail.com>
2026-09-24 23:58:16 -0300

    Making the robot work

--- andino_bringup/launch/camera.launch.py
+++ andino_bringup/launch/camera.launch.py
@@ -30,32 +30,58 @@
 
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, TextSubstitution
-from ament_index_python.packages import get_package_share_directory
-from os.path import join
 
-pkg_andino_bringup = get_package_share_directory('andino_bringup')
 
 def generate_launch_description():
-    # Declare launch argument for the path to the camera params YAML file (the 'file://' part is mandatory, you can't skip it)
-    intrinsic_params_file = DeclareLaunchArgument(
-        'intrinsic_params_file',
-        default_value='file://' + join(pkg_andino_bringup, 'config', 'raspicam.yaml'),
-        description='Path to camera intrinsics YAML file'
-    )
-
     return LaunchDescription([
-        intrinsic_params_file,
         Node(
-            package='v4l2_camera',
-            executable='v4l2_camera_node',
-            name='v4l2_camera_node',
+            package='camera_ros',
+            executable='camera_node',
+            name='camera',
             output='screen',
             parameters=[{
-                'image_size': [640, 480],
-                'camera_frame_id': 'camera_link',
-                'camera_info_url': LaunchConfiguration('intrinsic_params_file'),
+                'width': 640,
+                'height': 360,
+                'frame_id': 'camera_link',
             }],
+            remappings=[
+                ('/camera/image_raw', '/image_raw'),
+                ('/camera/image_raw/compressed', '/image_raw/compressed'),
+                ('/camera/camera_info', '/camera_info'),
+            ],
         )
     ])
+
+
+
+#from launch import LaunchDescription
+#from launch_ros.actions import Node
+#from launch.actions import DeclareLaunchArgument
+#from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, TextSubstitution
+#from ament_index_python.packages import get_package_share_directory
+#from os.path import join
+
+#pkg_andino_bringup = get_package_share_directory('andino_bringup')
+
+#def generate_launch_description():
+#    # Declare launch argument for the path to the camera params YAML file (the 'file://' part is mandatory, you can't skip it)
+#    intrinsic_params_file = DeclareLaunchArgument(
+#        'intrinsic_params_file',
+#        default_value='file://' + join(pkg_andino_bringup, 'config', 'raspicam.yaml'),
+#        description='Path to camera intrinsics YAML file'
+#    )
+
+#    return LaunchDescription([
+#        intrinsic_params_file,
+#        Node(
+#            package='v4l2_camera',
+#            executable='v4l2_camera_node',
+#            name='v4l2_camera_node',
+#            output='screen',
+#            parameters=[{
+#                'image_size': [640, 480],
+#                'camera_frame_id': 'camera_link',
+#                'camera_info_url': LaunchConfiguration('intrinsic_params_file'),
+#            }],
+#        )
+#    ])
